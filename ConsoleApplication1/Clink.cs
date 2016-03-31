b1043f4c48b69c8e2576b51977eade4b2fc1fde7
toadhats <limule@icloud.com>
2016-03-31 21:57:35 +1100

    finally able to fetch data and create a corresponding object

---
+++ ConsoleApplication1/Clink.cs
@@ -0,0 +1,13 @@
+﻿class Clink
+{
+    public string _id;
+    public string officeType { get; set; }
+    public string siteName { get; set; }
+    public string altName { get; set; }
+    public string address { get; set; }
+    public string suburb { get; set; }
+    public string state { get; set; }
+    public string postcode { get; set; }
+    public string latitude { get; set; }
+    public string longitude { get; set; }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                // TODO - Send HTTP requests
+                client.BaseAddress = new Uri("http://data.gov.au/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/action/datastore_search?resource_id=5a45d7b2-8579-425b-bb46-53a0e0bfa053&limit=1");
+                if (response.IsSuccessStatusCode)
+                {
+                    JObject json = JObject.Parse(await response.Content.ReadAsStringAsync());
+                    IList<JToken> records = json["result"]["records"].Children().ToList();
+                    foreach(JToken record in records)
+                    {
+                        Clink clink = JsonConvert.DeserializeObject<Clink>(record.ToString());
+                        Console.WriteLine("{0}\t{1}\t{2}", clink._id, clink.siteName, clink.postcode);
+                    }
+                        
+                }
+            }
+        }
+    }
+}
