ed0265a1d3252837a640bd16190c68b6934e42a4
toadhats <limule@icloud.com>
2016-03-31 23:03:46 +1100

    worked out how to use LINQ
    to query/filter/sort data. LINQ can also be used when data is being retrieved probably - it allows us to do SQL style queries on the working copies of the data, instead of querying the tables service. This might mean that the best approach when filtering results finely is to grab a coarser subset of the data from the azure table and finish digesting it in the controller layer or w/e.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -27,16 +27,28 @@ namespace JSONApiConsumer
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                HttpResponseMessage response = await client.GetAsync("api/action/datastore_search?resource_id=5a45d7b2-8579-425b-bb46-53a0e0bfa053&limit=1");
+                HttpResponseMessage response = await client.GetAsync("api/action/datastore_search?resource_id=5a45d7b2-8579-425b-bb46-53a0e0bfa053&limit=1000");
                 if (response.IsSuccessStatusCode)
                 {
                     JObject json = JObject.Parse(await response.Content.ReadAsStringAsync());
                     IList<JToken> records = json["result"]["records"].Children().ToList();
+                    IList< Clink > offices = new List<Clink>();
                     foreach(JToken record in records)
                     {
                         Clink clink = JsonConvert.DeserializeObject<Clink>(record.ToString());
-                        Console.WriteLine("{0}\t{1}\t{2}", clink._id, clink.siteName, clink.postcode);
+                        Console.WriteLine("{0}\t-\t{1}", clink._id, clink.siteName, clink.postcode);
+                        offices.Add(clink);
                     }
+                    Console.WriteLine("VICTORIA:\n-------------------------------");
+                    var inVic = (from office in offices
+                                 where office.state == "VIC"
+                                 orderby office.postcode ascending
+                                 select new { office.siteName, office.postcode });
+                    foreach(var vicOffice in inVic)
+                    {
+                        Console.WriteLine("{0}\t-\t{1}",vicOffice.postcode, vicOffice.siteName);
+                    }
+                    Console.WriteLine("{0} offices found in Victoria", inVic.Count());
                         
                 }
             }
--- ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("JSON-API-consumer.Tests")]
+[assembly: InternalsVisibleTo("ConsoleApplication1.Explorables")]
+
